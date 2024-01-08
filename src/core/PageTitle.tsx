361656d368166d43ebf3df1edfc0c64087618436
Andreas Bratfisch <duskman72@gmail.com>
2024-01-08 13:39:15 +0100

    add back button to DockerPage and PageTitle components

--- src/core/PageTitle.tsx
+++ src/core/PageTitle.tsx
@@ -5,16 +5,20 @@ import React, {
     useContext,
     useState,
 } from 'react';
+import {useNavigate} from "@k8s-cloud-io/react-router";
 
 export const PageTitleContext = createContext<{
     pageTitle: string | null;
+    backLink: string | null;
     setPageTitle: (title: string) => void;
+    setBackLink: (link: string) => void;
 }>(undefined);
 
 export const PageTitleContextProvider = (props: PropsWithChildren) => {
     const [pageTitle, setPageTitle] = useState(undefined);
+    const [backLink, setBackLink] = useState(undefined);
     return (
-        <PageTitleContext.Provider value={{ pageTitle, setPageTitle }}>
+        <PageTitleContext.Provider value={{ pageTitle, setPageTitle, backLink, setBackLink }}>
             {props.children}
         </PageTitleContext.Provider>
     );
@@ -26,5 +30,16 @@ export const usePageTitle = () => {
 
 export const PageTitle = memo(() => {
     const ctx = usePageTitle();
-    return <h5 className={'page-title'}>{ctx.pageTitle}</h5>;
+    const navigate = useNavigate();
+    if( ctx.backLink ) {
+        return <h5 className={'page-title'}>
+            <span className={'material-icons-outlined cursor-pointer fs-4 me-2'} onClick={() => {
+                navigate(ctx.backLink)
+            }}>arrow_back</span>
+            <span>{ctx.pageTitle}</span>
+        </h5>;
+    }
+    return <h5 className={'page-title'}>
+        <span>{ctx.pageTitle}</span>
+    </h5>;
 });
--- src/pages/docker/DockerPage.tsx
+++ src/pages/docker/DockerPage.tsx
@@ -2,9 +2,10 @@ import React, {PropsWithChildren} from "react";
 import {Container, usePageTitle} from "@core";
 import {DockerNavigation} from ".";
 
-export const DockerPage = (props: PropsWithChildren & {pageTitle: string}) => {
+export const DockerPage = (props: PropsWithChildren & {pageTitle: string, backLink?: string}) => {
     const ctx = usePageTitle();
     ctx.setPageTitle(props.pageTitle);
+    ctx.setBackLink(props.backLink);
 
     return <Container fluid className={'row m-0 mt-2 p-0'}>
         <DockerNavigation />
