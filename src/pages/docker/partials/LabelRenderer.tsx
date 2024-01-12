3bb89ed2c21231f75beae67705c8dcf71eada6c3
Andreas Bratfisch <duskman72@gmail.com>
2024-01-12 09:30:20 +0100

    fix label renderer

--- src/pages/docker/partials/LabelRenderer.tsx
+++ src/pages/docker/partials/LabelRenderer.tsx
@@ -3,7 +3,7 @@ import {LabelObject} from "../props";
 
 export const LabelRenderer = (props: {labels: LabelObject}) => {
     return Object.keys(props.labels || {}).map( key => {
-        return <span className={'d-block'}>
+        return <span className={'d-block p-0'}>
             {key}: {props.labels[key]}
         </span>
     });
